d239d69e5a3304eeb55b9d96b406ddd701e20974
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-14 20:55:58 +0100

    Linking to each post after clicking Ver button

--- src/componentes/Router.js
+++ src/componentes/Router.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Header from "./Header";
 import Navegacion from "./Navegacion";
 import Posts from "./Posts";
+import SinglePost from "./SinglePost";
 
 class Router extends Component {
 
@@ -36,6 +37,15 @@ class Router extends Component {
                                 return (
                                     <Posts
                                         posts={this.state.posts}
+                                    />
+                                )
+                            }}
+                            />
+                            <Route exact path="/post/:postId" render={(props) => {
+                                let idPost = props.location.pathname.replace('/post/', '');
+                                return (
+                                    <SinglePost
+
                                     />
                                 )
                             }}
---
+++ src/componentes/SinglePost.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SinglePost extends Component{
+    render() {
+        return (
+            <p>
+                desde singlepost
+            </p>
+        );
+    }
+}
+
+export default SinglePost
