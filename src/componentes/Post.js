a7b7ec8c14fa49177aad889a8ca8ab1df0a18dc3
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-15 13:25:45 +0100

    Adding sweetalert library to Borrar button in Post component

--- src/componentes/Post.js
+++ src/componentes/Post.js
@@ -1,7 +1,34 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import Swal from 'sweetalert2';
 
 class Post extends Component {
+
+    confirmarEliminacion = () => {
+
+        const {id} = this.props.info;
+
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡Esta acción no se puede deshacer!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, ¡borrar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                this.props.borrarPost(id)
+                Swal.fire(
+                    '¡Eliminado!',
+                    'El post ha sido eliminado.',
+                    'success'
+                )
+            }
+        })
+    };
+
     render() {
 
         const {id, title} = this.props.info;
@@ -12,7 +39,7 @@ class Post extends Component {
                 <td>{title}</td>
                 <td>
                     <Link to={`/post/${id}`} className="btn btn-primary"> Ver</Link>
-                    <button onClick={ () => this.props.borrarPost(id)} type="button" className="btn btn-danger">Borrar</button>
+                    <button onClick={this.confirmarEliminacion} type="button" className="btn btn-danger">Borrar</button>
                 </td>
             </tr>
         );
